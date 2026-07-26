5d9faa6bf95c0ab64295e6e2bc8758d8edbe0d65
suji-gelam <sujithagelam@gmail.com>
2026-07-26 13:55:12 +0530

    chanege localhost to cloud

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,7 +4,7 @@ import Stripe from "stripe";
 //placing user order from frontendd
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5174";
+  const frontend_url = "https://food-delivery-frontend-swart-omega.vercel.app/";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
